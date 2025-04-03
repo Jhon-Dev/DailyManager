d4af714c5cdf7776e18f9122e56a5b47e45e78e9
Jhonatan Nuss <contato@jhonatanuss.dev>
2025-04-03 10:20:36 -0300

    refactor: remove bin/www e usa app.js como entrypoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,14 @@
 
 import axios from 'axios';
 
+// const api = axios.create({
+//   baseURL: 'http://localhost:3001',
+// });
+
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: "https://daily-backend.onrender.com", // exemplo
 });
+
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
